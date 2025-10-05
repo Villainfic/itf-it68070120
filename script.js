2bd5965d083567fea316ea582fc706fad8119e7f
fleuriste <164160139+Villainfic@users.noreply.github.com>
2025-10-05 18:24:12 +0700

    68070120

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const myinput = document.getElementById("myinput")
+let count = 0;
+let totalacc = 0;
+let totalcash = 0;
 
-function add() {
-    myinput.value = Number(myinput.value) + 1
+function change() {
+    const acc = document.getElementById("acc");
+    const cash = document.getElementById("cash");
+    const historyBox = document.getElementById("history");
+
+    count++;
+    // อัปเดตค่าเริ่มต้นจาก input แต่เป็น number
+    totalacc = Number(acc.value);
+    totalcash = Number(cash.value);
+
+    let log = `${count} | Current account balance: ${totalacc} | Current cash balance: ${totalcash}\n`;
+    historyBox.textContent += log;
+    console.log(totalacc, totalcash);
+}
+
+function proceed() {
+    const DepoWith = document.getElementById("DepoWith");
+    const DepoNum = document.getElementById("DepoNum");
+    const historyBox = document.getElementById("history");
+    
+    count++;
+    let DepoW = DepoWith.value;
+    let DepoN = Number(DepoNum.value);
+
+    if (DepoW === "deposit") {
+        totalacc += DepoN;
+        totalcash -= DepoN;
+    } else {
+        totalacc -= DepoN;
+        totalcash += DepoN;
+    }
+
+    let log = `${count} | Current account balance: ${totalacc} | Current cash balance: ${totalcash}\n`;
+    historyBox.textContent += log;
 }
